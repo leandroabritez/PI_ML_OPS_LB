c68fecb64043926fdda5eb669db69f270c2f916d
LBritez <leandroabritez@gmail.com>
2024-01-30 12:57:23 -0300

    update 30-1

--- README.md
+++ README.md
@@ -3,9 +3,35 @@ Repositorio de Proyecto Individual 01 para Henry
 
 Ciclo completo de proyecto de Machine Learning 
 
-# Empresa: STEAM
+## Empresa: STEAM
  Plataforma multinacional de videojuegos. 
 
-# Objetivo 
+## Objetivo 
 Crear un sistema de recomendación de videojuegos para usuarios.
 
+
+## Extracción de datos 
+
+La base de datos se puede descargar del siguiente link: 
+https://drive.google.com/drive/folders/1HqBG2-sUkz_R3h1dZU5F2uAzpRn7BSpj
+
+Donde se encuentran 3 archivos comprimidos con WinRar, que alojan a archivos en formato JSON cada uno. 
+- user_items.json.gz
+- user_reviews.json.gz
+- steam_games.json.gz
+
+Se descomprimen los archivos manualmente.
+
+se usan diferentes metodos para leer los .JSON en cada caso, dado que algunos archivos no tienen las seperaciones estandar con doble comilla '', ergo, se debe utilizar una lectura literal con el modulo ast para poder leer el archivo. 
+
+### Analasis primario (EDA)
+
+Se realiza un analisis golbal, mostrando los encabezados de los dataframes para entender mejor su disposición. Se eliminan aquellas filas donde todos los datos sean nulos, se verifica si alguna columna es redudnate para ser eliminada tambien. 
+
+
+
+
+### Extraccones en JSON 
+
+Identificar los diferentes endpoints que me piden, para analizar las columnas necesarias y descartar las que no se usen. Para de esta forma trabajar con archivos más livianos. 
+De igual forma, verificar antes de eliminar las columnas, que ninguna de ellas sea de aparente utilidad para los endpoints que son entrenados con machine learning. 
--- main.py
+++ main.py
@@ -26,3 +26,52 @@ def PlayTimeGenre(genero: str):
     else:
         raise HTTPException(status_code=404, detail=f"No hay información para el género '{genero}'.")
 
+
+
+
+# Definir la función developer
+@app.get("/developer/{developer}")
+def developer(developer: str):
+
+    data = pd.read_csv('../PI_ML_OPS_data util/steam_games.csv')
+
+    # Filtrar por el desarrollador
+    developer_data = data[data['developer'] == developer]
+
+    # Crear una lista para almacenar los DataFrames de cada año
+    dfs = []
+
+    # Obtener años únicos
+    unique_years = developer_data['year'].unique()
+
+    for year in unique_years:
+        # Filtrar por año
+        year_data = developer_data[developer_data['year'] == year]
+
+        # Contar la cantidad de items
+        item_count = len(year_data)
+
+        # Calcular el contenido free
+        free_content_percentage = (year_data['price'].fillna(0) == 0).mean() * 100
+
+        # Crear un DataFrame para el año actual solo si hay registros
+        if item_count > 0:
+            year_df = pd.DataFrame({
+                'año': [year],
+                'cantidad de items': [item_count],
+                'contenido free': [free_content_percentage]
+            })
+
+            # Agregar el DataFrame al a lista
+            dfs.append(year_df)
+
+    # Verificar si hay DataFrames en la lista antes de concatenar
+    if dfs:
+        # Concatenar todos los DataFrames en uno solo
+        result_df = pd.concat(dfs, ignore_index=True)
+    else:
+        # Si no hay registros, crear un DataFrame vacío
+        result_df = pd.DataFrame(columns=['año', 'cantidad de items', 'contenido free'])
+
+    return print(result_df)
+
